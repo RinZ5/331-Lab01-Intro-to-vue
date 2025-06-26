d17a781c04ed0b400fc3d283a6b0cdeed323bbe9
RinZ <supawit.promma6256@gmail.com>
2025-06-26 23:50:25 +0700

    complete 8.4

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
   setup(){
     const product = ref('Boots')
-    const image = ref('./assets/images/socks_green.jpg')
-    const inStock = ref(false)
+    const brand = ref('SE 331')
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image
+    })
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity
+    })
     const inventory = ref(100)
     const details = ref([
       '50% cotton',
@@ -12,21 +17,32 @@ createApp({
       '20% polyester'
     ])
     const variants = ref([
-      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 }
     ])
+    const selectedVariant = ref(0)
     const cart = ref(0)
 
     function addToCart(){
       cart.value += 1
     }
 
+    const title = computed(() => {
+      return brand.value + ' ' + product.value
+    })
+
     function updateImage(variantImage){
       image.value = variantImage
     }
 
+    function updateVariant(index){
+      selectedVariant.value = index
+    }
+
     return {
       product,
+      brand,
+      title,
       image,
       inStock,
       inventory,
@@ -34,7 +50,8 @@ createApp({
       variants,
       cart,
       addToCart,
-      updateImage
+      updateImage,
+      updateVariant
     }
   }
 }).mount('#app')
