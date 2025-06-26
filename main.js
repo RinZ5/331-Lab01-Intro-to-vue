7e73457496a3d62cc763dbead0131dce7e5b6479
RinZ <supawit.promma6256@gmail.com>
2025-06-27 00:12:34 +0700

    complete 9.8

--- main.js
+++ main.js
@@ -1,57 +1,17 @@
 const { createApp, ref, computed } = Vue
 
-createApp({
+const app = createApp({
   setup(){
-    const product = ref('Boots')
-    const brand = ref('SE 331')
-    const image = computed(() => {
-      return variants.value[selectedVariant.value].image
-    })
-    const inStock = computed(() => {
-      return variants.value[selectedVariant.value].quantity
-    })
-    const inventory = ref(100)
-    const details = ref([
-      '50% cotton',
-      '30% wool',
-      '20% polyester'
-    ])
-    const variants = ref([
-      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 }
-    ])
-    const selectedVariant = ref(0)
     const cart = ref(0)
-
-    function addToCart(){
-      cart.value += 1
-    }
-
-    const title = computed(() => {
-      return brand.value + ' ' + product.value
-    })
-
-    function updateImage(variantImage){
-      image.value = variantImage
-    }
-
-    function updateVariant(index){
-      selectedVariant.value = index
-    }
+    const premium = ref(true)
 
     return {
-      product,
-      brand,
-      title,
-      image,
-      inStock,
-      inventory,
-      details,
-      variants,
       cart,
-      addToCart,
-      updateImage,
-      updateVariant
+      premium
     }
   }
-}).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+
+app.mount('#app')
