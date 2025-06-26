94410cb231a13a72dd700ee4804379c5ac557281
RinZ <supawit.promma6256@gmail.com>
2025-06-26 22:23:07 +0700

    complete 3.5

---
+++ main.js
@@ -0,0 +1,13 @@
+const { createApp, ref } = Vue
+
+createApp({
+  setup(){
+    const product = ref('Boots')
+    const image = ref('./assets/images/socks_green.jpg')
+
+    return {
+      product,
+      image
+    }
+  }
+}).mount('#app')
